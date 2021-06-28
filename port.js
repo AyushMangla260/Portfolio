c1d4faa3239036e4ad9e0df754c46f87809c7fb7
Ayush Mangla <aayushmangla260@gmail.com>
2021-06-28 13:41:18 +0530

    Initial commit

---
+++ port.js
@@ -0,0 +1,14 @@
+function linkedin(){
+    window.open('https://www.linkedin.com/in/ayush-mangla-9a1a8b1a5', '_blank')};
+  
+function instagram(){
+      window.open('https://www.instagram.com/ayush.mangla_/', '_blank')};
+function facebook(){
+  window.open('https://www.facebook.com/profile.php?id=100005969476223', '_blank');
+}
+function google(){
+  window.open('mailto:aayushmangla260@gmail.com','_blank');
+}
+function github(){
+    window.open('https://github.com/AyushMangla260','_blank');
+}
